98f055b2294be29fb7c25f7b676b9e694d2c40d7
pj <Pinjiang1220@gmail.com>
2025-07-11 02:39:33 +0800

    feat: prevent race condtion when using SAM

--- models/sam/main.py
+++ models/sam/main.py
@@ -1,15 +1,27 @@
 import asyncio
 import json
 import os
-import signal
+from typing import List
 
 import nats
 from events import SAMPredictEvent
 from nats.aio.client import Client
 from predictor import InferenceAPI
+from ultralytics.utils import ThreadingLocked
 
 servers = os.environ.get("NATS_URL", "nats://localhost:4222").split(",")
+
 inference_api = InferenceAPI()
+
+
+@ThreadingLocked()
+def thread_safe_predict(
+    image_paths: List[str],
+    points: List[List[List[int]]],
+    labels: List[List[int]],
+):
+    results = inference_api.predict(image_paths, points, labels)
+    return results
 
 
 async def main():
@@ -19,29 +31,35 @@ async def main():
 
     # Handle incoming messages
     async for msg in sub.messages:
-        event = SAMPredictEvent.model_validate_json(msg.data)
-        print("Received event:")
-        print(event)
+        try:
+            event = SAMPredictEvent.model_validate_json(msg.data)
+            print("Received event:")
+            print(event)
 
-        results = inference_api.predict([event.image_url], event.points, event.labels)
+            results = thread_safe_predict(
+                [event.image_url],
+                event.points,
+                event.labels,
+            )
 
-        boxes = []
-        masks = []
-        for result in results:
-            if result.boxes is not None:
-                boxes.append(result.boxes.xyxy.flatten().tolist())
-            if result.masks is not None:
-                curMask = []
-                for segs in result.masks.xy:
-                    for coords in segs:
-                        curMask.append(coords.flatten().tolist())
-                masks.append(curMask)
-        reply = {
-            "boxes": boxes,
-            "masks": masks,
-        }
-
-        await msg.respond(json.dumps(reply).encode("utf-8"))
+            boxes = []
+            masks = []
+            for result in results:
+                if result.boxes is not None:
+                    boxes.append(result.boxes.xyxy.flatten().tolist())
+                if result.masks is not None:
+                    curMask = []
+                    for segs in result.masks.xy:
+                        for coords in segs:
+                            curMask.append(coords.flatten().tolist())
+                    masks.append(curMask)
+            reply = {
+                "boxes": boxes,
+                "masks": masks,
+            }
+            await msg.respond(json.dumps(reply).encode("utf-8"))
+        except Exception as e:
+            await msg.respond(json.dumps({"error": str(e)}).encode("utf-8"))
 
     await nc.drain()
 
